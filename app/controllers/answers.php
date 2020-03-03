c89a974b83e403fc4c7a5c6de8e608de8000fa7c
khaled301 <khaledhasan301@gmail.com>
2020-03-03 17:24:37 +0600

    Update Answer by User

--- app/controllers/answers.php
+++ app/controllers/answers.php
@@ -7,10 +7,15 @@ use Models\Answer;
 class Answers {
 
     public static function add_answer($answer, $user_id, $question_id) {
-
         $answer = Answer::create(['answer'=>$answer, 'user_id'=>$user_id, 'question_id'=> $question_id]);
         return $answer;
+    }
 
+    public static function update_answer($answer_id, $new_answer) {
+        $answer = Answer::find($answer_id);
+        $answer->answer = $new_answer;
+        $updated_answer = $answer->save();
+        return $updated_answer;
     }
 
 }
--- index.php
+++ index.php
@@ -10,41 +10,33 @@ use Controllers\Upvotes;
 /*
 // Import user controller
 $user = Users::create_user("Khaled", "khaled@example.com","password");
-*/
 
-/*
 //Import question controller
 $question = Questions::create_question("Is this your first time doing Eloquent ORM?", 1);
-*/
-/*
+
 //Import answer controller
 $answers = Answers::add_answer("This is the first answer!", 1, 1);
-*/
 
-/*
 //Import upvote controller
 //$answers = Upvotes::upvote_answer(1, 1);
-*/
 
-/*
 //Retrieves the Questions with their corresponding answers
 $all_questions_with_answers = Questions::get_questions_with_answer();
 print_r($all_questions_with_answers);
-*/
 
-/*
 //Retrieve question with user
 $all_questions_with_user = Questions::get_questions_with_users();
 print_r($all_questions_with_user);
-*/
 
-/*
 // Get One Question With Answers and Upvotes
 $one_question = Questions::get_question_answers_upvotes(1);
 print_r($one_question);
-*/
-
 
 // Count All Questions by a Particular User
 $user_question_count = Users::question_count(1);
 print_r($user_question_count);
+
+
+*/
+//Update Answer by User
+$update_answer = Answers::update_answer(1,"This is an updated answer");
